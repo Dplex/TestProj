27f8f3133416311fe2f3ef017d3bdfb92d38ff59
dplex <dplex@outlook.com>
2016-01-01 16:59:51 +0900

    switching

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -3,5 +3,15 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
 	}
 }
